4c6c63f364f3170c350734e2ddfaa3fdbce9f4eb
GYegor <yahor2011@gmail.com>
2019-02-24 22:02:35 +0300

    feat: add implementation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var arr = str.split('');
+      for(var j = 0; j < arr.length-1; j++) {
+        if (arrN(arr) === false) return false; 
+        if (arr[j+1] == bracketsConfig[arrN(arr)][1]) {
+          arr.splice(j, 2);   
+          if (arr.length == 0) return true;
+          if (arr.length == 1) return false;
+          j = -1;
+        }
+      } 
+      return false;
+function arrN(arr) {
+  for(var i = 0; i < bracketsConfig.length; i++) {
+    if (arr[j] == bracketsConfig[i][0]) {
+      return i;}
+    }
+   return false;
 }
+};
+
